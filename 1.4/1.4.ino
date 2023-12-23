bd0e8aa4dcd6cd993a6ff8e52d8b277f8d0ca753
Michael-she <mgshepstone@gmail.com>
2023-12-23 23:10:34 +0200

    Can do multiple probes

--- 1.4/1.4.ino
+++ 1.4/1.4.ino
@@ -1,12 +1,17 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino.h>
-const int analogPin = 36;
 
+int currentTransformers[] = {36, 28, 34, 35, 32, 33};
+
+int numCurrentTransformers = 2;
 
 const int red = 12;
 const int green = 14;
 const int blue = 27;
+
+
+
 const int resistor = 122;
 const int transformerMultiplier = 2000;
 
@@ -45,43 +50,66 @@ for(int i = 0; i<10; i++){
     delay(50);
     digitalWrite(green, LOW);
 }
-   pinMode(analogPin, INPUT);
+
+  for(int i = 0; i<numCurrentTransformers; i++){
+   pinMode(currentTransformers[i], INPUT);
+  }
 }
 
 void loop() {
  
-   String toSend = id + ",";
+   String toSend = id + "";
     
 
-  float maxAmps = 0;
-  float minAmps = 999999;
-  double ampsSum = 0;
+  float maxAmps[numCurrentTransformers];
+  float minAmps[numCurrentTransformers];
+  
+  double ampsSum[numCurrentTransformers];
+
+
+ for(int i = 0; i<numCurrentTransformers; i++){
+   maxAmps[i] = 0;
+   minAmps[i] = 99999999;
+   ampsSum[i]=0;
+ }
 
   digitalWrite(green, HIGH);
     for(int i = 0; i<sendInterval*10; i++){
-
-      float currentAmps = getAmps(analogPin);
-      if(currentAmps>maxAmps){
-        maxAmps = currentAmps;
+       for(int i = 0; i<numCurrentTransformers; i++){
+      float currentAmps = getAmps(currentTransformers[i]);
+      
+      if(currentAmps>maxAmps[i]){
+        maxAmps[i] = currentAmps;
       }
 
-      if(currentAmps<minAmps){
-        minAmps = currentAmps;
+      if(currentAmps<minAmps[i]){
+        minAmps[i] = currentAmps;
       }
-    ampsSum +=currentAmps;
+    ampsSum[i] +=currentAmps;
+     }
+
+     Serial.println();
       delay(100);
 
 
     }
 digitalWrite(green, LOW);
-    float aveAmps = ampsSum/(sendInterval*10);
+
+for(int i = 0; i<numCurrentTransformers; i++){
+  String toSend = id + "";
+    float aveAmps = ampsSum[i]/(sendInterval*10);
 
     float power = aveAmps*220*sendInterval/3600;
+    if(numCurrentTransformers>1){
+      toSend+="-"+String(i)+",";
+    }else{
+      toSend+=",";
+    }
 
-    toSend+=toAmpString(aveAmps)+","+toAmpString(maxAmps)+","+toAmpString(minAmps)+","+toAmpString(power);
+    toSend+=toAmpString(aveAmps)+","+toAmpString(maxAmps[i])+","+toAmpString(minAmps[i])+","+toAmpString(power);
     Serial.println(toSend);
     sendPackets(toSend);
-
+}
  
 }
 
@@ -113,14 +141,15 @@ digitalWrite(blue, LOW);
     } else {
       Serial.print("Error on sending POST: ");
       Serial.println(httpResponseCode);
-      for(int i = 0; i<10; i++){
+      for(int i = 0; i<100; i++){
   delay(50);
     digitalWrite(red, HIGH);
     digitalWrite(blue, LOW);
     delay(50);
-      digitalWrite(red, HIGH);
-    digitalWrite(green, LOW);
+      digitalWrite(red, LOW);
+    digitalWrite(blue, HIGH);
 }
+digitalWrite(blue, LOW);
     }
 
     https.end(); //End HTTP
@@ -153,8 +182,8 @@ int analogValue = analogRead(inPin);
   Serial.print(voltage);
   Serial.print("  ");
   float amps = (voltage/resistor) *transformerMultiplier*2;
- Serial.println(amps);
-
+ Serial.print(amps);
+Serial.print("  |  ");
   // Wait for a bit before reading again
 
 
